1cc5ca0445f4e50f883ddce95759a6edcd6e1804
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-07-31 22:27:08 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 const Button = (props) => {
-  // your code here
+  const { reset } = props;
+  return (
+    <div>
+      <button onClick={reset}>RESET</button>
+    </div>
+  );
 };
 
 const Application = () => {
-
   // your code here
+  const [name, setName] = useState();
 
   const reset = () => {
     console.log("reset");
     // your code here
+    if (name) {
+      setName(" ");
+    }
   };
 
   return (
     <main>
       {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="Type Your Name"
+      ></input>
+      <Button reset={reset} />
+      {name && <h1>Hello {name}</h1>}
+      {!name && <h1></h1>}
     </main>
   );
 };
