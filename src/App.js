d4742c04a9c50b55160efa72c3f996b7f86daf7c
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-08-05 20:22:15 -0600

    gutted react app for practice

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Nav from './components/Nav';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>YOOOOO</h1>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,12 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const Button = (props) => {
-  const { reset } = props;
-  return (
-    <div>
-      <button onClick={reset}>RESET</button>
-    </div>
-  );
-};
-
-const Application = () => {
-  // your code here
-  const [name, setName] = useState();
-
-  const reset = () => {
-    console.log("reset");
-    // your code here
-    if (name) {
-      setName(" ");
-    }
-  };
-
-  return (
-    <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <input
-        value={name}
-        onChange={(event) => setName(event.target.value)}
-        placeholder="Type Your Name"
-      ></input>
-      <Button reset={reset} />
-      {name && <h1>Hello {name}</h1>}
-      {!name && <h1></h1>}
-    </main>
-  );
-};
-
-ReactDOM.render(<Application />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
