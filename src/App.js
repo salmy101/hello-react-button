6c1ddf95f6236c3ef5227f7b98c8af3498f65e96
Salma Ibrahim <salmaibrahim@Salmas-Air.hitronhub.home>
2022-08-06 00:49:53 -0600

    add styled-components npm, react-router, made nav component, and routes to multi-pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import './App.scss';
-import Nav from './components/Nav';
+import React, { Component } from "react";
+import "./index.css";
+import Nav from "./components/Nav";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>YOOOOO</h1>
-    </div>
-  );
-};
+//pages
+import MainPage from "./pages/index";
+import About from "./pages/about";
+import Search from "./pages/search";
+import Match from "./pages/match";
+import Login from "./pages/login";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/match" element={<Match />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+
+        </Routes>
+      </Router>
+    );
+  }
+}
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h2>This is the MainPage!</h2>
+    </div>
+  );
+};
+
+export default MainPage;
